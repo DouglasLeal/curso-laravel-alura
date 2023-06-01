20692b742df167da1cb32fddabdbe44318d79b1f
Douglas Leal <douglas.leal@live.com>
2023-06-01 19:26:09 -0300

    Listando séries

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,5 +4,19 @@ namespace App\Http\Controllers;
 
 class SeriesController
 {
+    public function listarSeries(){
+        $series = [
+            'Punisher',
+            'Lost',
+            'Grey\'s Anatomy'
+        ];
 
+        $html = '<ul>';
+        foreach ($series as $serie){
+            $html .= "<li>$serie</li>";
+        }
+        $html .= '</ul>';
+
+        echo $html;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
-
+use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/series', function() {});
+Route::get('/series', [SeriesController::class, "listarSeries"]);
