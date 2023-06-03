8d2e7470f8c370e5434542f80148704faa9381fd
Douglas Leal <douglas.leal@live.com>
2023-06-03 13:04:11 -0300

    Alterando controller para usar model

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -8,7 +9,9 @@ class SeriesController
 {
     public function index(Request $request){
 
-        $series = DB::select('SELECT nome FROM series');
+        //$series = DB::select('SELECT nome FROM series');
+        //$series = Serie::all();
+        $series = Serie::query()->orderBy('nome')->get();
 
         return view('series.index')->with('series', $series);
     }
@@ -20,7 +23,11 @@ class SeriesController
     public function store(Request $request){
         $nome = $request->input('nome');
 
-        DB::insert('INSERT INTO series (nome) VALUES (?)', [$nome]);
+        //DB::insert('INSERT INTO series (nome) VALUES (?)', [$nome]);
+
+        $serie = new Serie();
+        $serie->nome = $nome;
+        $serie->save();
 
         return redirect('/series');
     }
