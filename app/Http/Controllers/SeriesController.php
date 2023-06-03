76585e545818f6e2afa931130ffe805746aa542a
Douglas Leal <douglas.leal@live.com>
2023-06-03 12:59:57 -0300

    alterando controller para usar db

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -1,22 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController
 {
     public function index(Request $request){
-        $series = [
-            'Punisher',
-            'Lost',
-            'Grey\'s Anatomy'
-        ];
+
+        $series = DB::select('SELECT nome FROM series');
 
         return view('series.index')->with('series', $series);
     }
 
     public function criar(Request $request){
         return view('series.create');
+    }
+
+    public function store(Request $request){
+        $nome = $request->input('nome');
+
+        DB::insert('INSERT INTO series (nome) VALUES (?)', [$nome]);
+
+        return redirect('/series');
     }
 }
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,5 +1,6 @@
 <x-layout title="Nova Série">
-    <form action="" method="post">
+    <form action="/series/salvar" method="post">
+        @csrf
         <label class="fw-bold mb-1" for="nome">Nome:</label>
         <input class="form-control mb-3" type="text" id="nome" name="nome">
         <input type="submit" value="Adicionar" class="btn btn-primary w-100">
