a7458e56de317330d1647f0405b5c05bfa7e9276
Douglas Leal <douglas.leal@live.com>
2023-06-05 20:24:33 -0300

    ajuste nas rotas

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/series', [SeriesController::class, "index"]);
-Route::get('/series/novo', [SeriesController::class, "criar"]);
-Route::post('/series/salvar', [SeriesController::class, "store"]);
+//Route::get('/series', [SeriesController::class, "index"]);
+//Route::get('/series/novo', [SeriesController::class, "criar"]);
+//Route::post('/series/salvar', [SeriesController::class, "store"]);
+
+Route::controller(SeriesController::class)->group(function(){
+    Route::get('/series', "index");
+    Route::get('/series/novo', "criar");
+    Route::post('/series/salvar', "store");
+});
