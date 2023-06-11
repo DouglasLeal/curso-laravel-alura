a86f28cd0cf5028db80cc1f756f56a8ce963b27f
Douglas Leal <douglas.leal@live.com>
2023-06-11 17:07:37 -0300

    models Episode e Season criados

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -5,13 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Serie extends Model
+class Episode extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['nome'];
-
-    public function temporadas(){
-        return $this->hasMany(Season::class);
-    }
 }
